e649fde9a2dd110cfeb73b1b3ce722efa33f4cd6
Liam Chen <z3278107@unsw.edu.au>
2023-05-18 02:32:29 +1000

    fix lighthouse accessibilities warnings

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Layout from './Layout';
-import { GithubIcon, YoutubeIcon, LinkedInIcon } from './Icons';
-import { motion } from 'framer-motion';
+import React from "react";
+import Layout from "./Layout";
+import { GithubIcon, YoutubeIcon, LinkedInIcon } from "./Icons";
+import { motion } from "framer-motion";
 
 const Footer = () => {
   return (
@@ -12,10 +12,20 @@ const Footer = () => {
         <div className="max-w-7xl mx-auto py-4 sm:px-6 lg:px-8 text-dark">
           <div className="grid grid-cols-1 md:grid-cols-7 gap-2 ">
             <div className="col-span-2">
-              <input id="first" type="text" placeholder="first name" className="h-10 block w-full rounded-md pl-2" />
+              <input
+                id="first"
+                type="text"
+                placeholder="first name"
+                className="h-10 block w-full rounded-md pl-2"
+              />
             </div>
             <div className="col-span-2">
-              <input id="last" type="text" placeholder="last name" className="h-10 block w-full rounded-md pl-2" />
+              <input
+                id="last"
+                type="text"
+                placeholder="last name"
+                className="h-10 block w-full rounded-md pl-2"
+              />
             </div>
             <div className="col-span-2">
               <input
@@ -27,7 +37,10 @@ const Footer = () => {
               />
             </div>
             <div>
-              <button type="submit" className=" py-2 px-6 bg-green-500 text-dark font-medium rounded-md shadow-md">
+              <button
+                type="submit"
+                className=" py-2 px-6 bg-green-500 text-dark font-medium rounded-md shadow-md"
+              >
                 Submit
               </button>
             </div>
@@ -36,27 +49,33 @@ const Footer = () => {
 
         <nav className="flex items-center justify-center flex-wrap space-x-2">
           <motion.a
+            aria-label="github"
             href="https://github.com/ligam0414"
-            target={'_blank'}
+            target={"_blank"}
             whileHover={{ y: -4 }}
             whileTap={{ scale: 0.9 }}
-            className="w-6">
+            className="w-6"
+          >
             <GithubIcon />
           </motion.a>
           <motion.a
+            aria-label="youtube"
             href="https://www.youtube.com/channel/UC32ldb54TTW_ObW1X167Ubg"
-            target={'_blank'}
+            target={"_blank"}
             whileHover={{ y: -4 }}
             whileTap={{ scale: 0.9 }}
-            className="w-6">
+            className="w-6"
+          >
             <YoutubeIcon />
           </motion.a>
           <motion.a
+            aria-label="linkedin"
             href="https://www.linkedin.com/in/liam-c-625565170/"
-            target={'_blank'}
+            target={"_blank"}
             whileHover={{ y: -4 }}
             whileTap={{ scale: 0.9 }}
-            className="w-6">
+            className="w-6"
+          >
             <LinkedInIcon />
           </motion.a>
         </nav>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useRef } from 'react';
-import { motion } from 'framer-motion';
-import { PlayIcon, PauseIcon } from './Icons';
+import React, { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import { PlayIcon, PauseIcon } from "./Icons";
 
 const Logo = ({ className }) => {
   const [play, setPlay] = useState(false);
@@ -25,25 +25,34 @@ const Logo = ({ className }) => {
     <div className={`flex items-center justify-center mt-2 ${className}`}>
       <motion.button
         onClick={toggleAudio}
+        aria-label="music"
         className="w-12 h-12 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold"
         whileHover={{
           scale: 1.2,
           backgroundColor: [
-            '#121212',
-            'rgba(131,58,180,1)',
-            'rgba(253,29,29,1)',
-            'rgba(252,176,69,1)',
-            'rgba(131,58,180,1)',
-            '#121212'
+            "#121212",
+            "rgba(131,58,180,1)",
+            "rgba(253,29,29,1)",
+            "rgba(252,176,69,1)",
+            "rgba(131,58,180,1)",
+            "#121212",
           ],
-          transition: { duration: 1, repeat: Infinity }
-        }}>
+          transition: { duration: 1, repeat: Infinity },
+        }}
+      >
         {!play ? <PlayIcon /> : <PauseIcon />}
       </motion.button>
       <div className="mx-4 flex">
-        <input type="range" className="mr-2 w-full accent-dark" min={0} max={MAX} onChange={(e) => handleVolume(e)} />
+        <input
+          aria-label="volume"
+          type="range"
+          className="mr-2 w-full accent-dark"
+          min={0}
+          max={MAX}
+          onChange={(e) => handleVolume(e)}
+        />
       </div>
-      <audio ref={musicRef} loop src={'/music.mp3'} />
+      <audio ref={musicRef} loop src={"/music.mp3"} />
     </div>
   );
 };
